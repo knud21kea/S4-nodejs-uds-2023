c82b8da504a07aeab54fd68355b953b9535ae3b4
knud21kea <grah0070@stud.kea.dk>
2023-02-07 17:09:33 +0100

    Hand in

--- 00._Miscellaneous/1.Intro_exercises/1.Basics.js
+++ 00._Miscellaneous/1.Intro_exercises/1.Basics.js
@@ -10,7 +10,7 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
-const textToShow = `My first name is ${firstName} and my last name is ${lastName}`;
+const textToShow = `My first name is ${firstName} and my last name is ${lastName}.`;
 console.log(textToShow);
 
 // --------------------------------------
