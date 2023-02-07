1f16cc7130c8bc6dd7b927f25b83f4eca97e31ef
knud21kea <grah0070@stud.kea.dk>
2023-02-07 14:57:41 +0100

    Objects solved

--- 00._Miscellaneous/1.Intro_exercises/3.Objects .js
+++ 00._Miscellaneous/1.Intro_exercises/3.Objects .js
@@ -7,10 +7,15 @@ const myObj = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
+console.log(myObj.message);
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
+
+const me = { name: "Graham", age: 60};
+console.log(me);
 
 
 // --------------------------------------
@@ -20,6 +25,8 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
@@ -27,5 +34,7 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
+thisSong.about = "Just a tribute";
 
 // --------------------------------------
