36ff361d875f19408082f4eeed64829339222f77
knud21kea <grah0070@stud.kea.dk>
2023-02-17 01:07:27 +0100

    Endpoint added to delete by id

--- 01._RestAPI_Birds_Part_1/app.js
+++ 01._RestAPI_Birds_Part_1/app.js
@@ -31,7 +31,12 @@ birds.push({
     description: "Columbidae have unique body feathers, with the shaft being generally broad, strong, and flattened, tapering to a fine point, abruptly."
 });
 
-//console.log(birds);
+/* test data for POST mapping in Postman
+{
+    "name" : "Magpie", "family" : "Corvidae", "size" : "45", "mass" : "240",
+    "description" : "The head, neck and breast are glossy black with a metallic green and violet sheen; the belly and scapulars (shoulder feathers) are pure white; the wings are black glossed with green or purple, and the primaries have white inner webs, conspicuous when the wing is open."
+}
+*/
 
 
 // ----------------------------------------------------------------------------------------------
@@ -42,33 +47,44 @@ const app = express();
 app.use(express.json()); // Future implementation of POST?
 
 
-//Endpoint for the API to display all birds via HTTP GET mapping
+//Endpoint for the API to read all birds via HTTP GET mapping
 app.get("/birds", (req, res) => {
     res.send(birds);
 });
 
 
-// Endpoint for the API to display one bird with 'id: "id"' via HTTP GET mapping
+// Endpoint for the API to read the bird with 'id: id' via HTTP GET mapping
 app.get("/birds/:id", (req, res) => {
     res.send(birds.find(bird => bird.id === parseInt(req.params.id))); // fails if no bird with that id
 });
 
 
-// Endpoint for the API to create one bird with 'id: "id"' via HTTP POST mapping
-app.post("/birds", (req, res) =>  {
-    birds.push({ 
-        id: birds.slice(-1)[0].id + 1, 
-        name: req.body.name, 
-        family: req.body.family, 
-        size: req.body.size, 
-        mass: req.body.mass, 
-        description: req.body.description});
-    res.send({ created:  birds.slice(-1)[0]});
+//Birds Rest API - Part 2
+
+
+// Endpoint for the API to create one bird with 'auto-incremented' id via HTTP POST mapping
+app.post("/birds", (req, res) => {
+    birds.push({
+        id: birds.slice(-1)[0].id + 1,
+        name: req.body.name,
+        family: req.body.family,
+        size: req.body.size,
+        mass: req.body.mass,
+        description: req.body.description
+    });
+    res.send({ created: birds.slice(-1)[0] });
 });
 
 
-app.delete("/birds/:id", (req, res) =>  {
-    res.send({ bird: req.body });
+// Endpoint for the API to delete the bird with 'id: id' via HTTP DELETE mapping
+app.delete("/birds/:id", (req, res) => {
+    const indexOfBirdToDelete = birds.indexOf(birds.find(bird => bird.id === parseInt(req.params.id)));
+    let birdDeleted = [];
+    if (indexOfBirdToDelete > -1) {
+        birdDeleted = birds.splice(indexOfBirdToDelete, 1);
+        console.log.birdDeleted;
+    }
+    res.send({ deleted: birdDeleted[0] });
 });
 
 
